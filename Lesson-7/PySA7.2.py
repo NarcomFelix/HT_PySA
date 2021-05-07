4a44f605a46d665af75bcf90e5a0a7cdabbbb7df
PavelZh <zidkihpv@gmail.com>
2021-05-07 15:55:02 +0300

    home task for PythonSA lesson-7

---
+++ Lesson-7/PySA7.2.py
@@ -0,0 +1,46 @@
+"""
+Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+"""
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 50
+data = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(data)
+
+def merge_sort(alist, start=0, end=SIZE):
+    if end - start > 1:
+        mid = (start + end) // 2
+        merge_sort(alist, start, mid)
+        merge_sort(alist, mid, end)
+        merge_list(alist, start, mid, end)
+
+def merge_list(alist, start, mid, end):
+    left = alist[start:mid]
+    right = alist[mid:end]
+    k = start
+    i = 0
+    j = 0
+    while start + i < mid and mid + j < end:
+        if left[i] <= right[j]:
+            alist[k] = left[i]
+            i = i + 1
+        else:
+            alist[k] = right[j]
+            j = j + 1
+        k = k + 1
+    if start + i < mid:
+        while k < end:
+            alist[k] = left[i]
+            i = i + 1
+            k = k + 1
+    else:
+        while k < end:
+            alist[k] = right[j]
+            j = j + 1
+            k = k + 1
+
+merge_sort(data)
+print(data)
